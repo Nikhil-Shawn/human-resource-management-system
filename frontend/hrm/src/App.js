55123ad48bea308ba861e7275c8d7ba987f0b787
Nikhil-Shawn <nikhilchavan081998@gmail.com>
2024-05-29 12:22:52 +0200

    Rework completed

---
+++ frontend/hrm/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
