192d138b97ab2c9ea414e9d60f1407bbc0829a44
anirudh17d <anirudhdb@gmail.com>
2024-05-29 16:59:15 +0200

    Added routing

--- frontend/hrm/src/index.js
+++ frontend/hrm/src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from './Pages/Dashboard';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
-    <App />
+    <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/dashboard" element={<Dashboard/>}></Route>
+    </Routes>
   </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
