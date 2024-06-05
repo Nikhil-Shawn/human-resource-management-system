d6d400adbb55961bf25a134506e588328feb2076
poojasanjaypatil <poojasanjayp11@gmail.com>
2024-06-05 16:50:07 +0200

    Education Multiple Post

--- src/main/java/com/humanresourcemanagementsystem/Controller/EducationController/EducationController.java
+++ src/main/java/com/humanresourcemanagementsystem/Controller/EducationController/EducationController.java
@@ -1,39 +1,40 @@
 package com.humanresourcemanagementsystem.Controller.EducationController;
 
-import com.humanresourcemanagementsystem.Dto.BenefitDTO;
 import com.humanresourcemanagementsystem.Dto.EducationDTO;
-import com.humanresourcemanagementsystem.Dto.ExperienceDTO;
 import com.humanresourcemanagementsystem.Service.EducationService;
-import com.humanresourcemanagementsystem.Service.ExperienceService;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin
 @RequestMapping("api/v1/education")
-public class EducationController
-{
+public class EducationController {
+
     @Autowired
     private EducationService educationService;
+
     @PostMapping("/save")
     public String saveEducation(@RequestBody EducationDTO educationDTO) {
         return educationService.addEducation(educationDTO);
     }
 
-//    // Get experience by ID
-//    @GetMapping("/{id}")
-//    public ExperienceDTO getExperienceById(@PathVariable int id) {
-//        return experienceService.getExperienceById(id);
-//    }
-//
-//    // Get all experiences
-//    @GetMapping("/all")
-//    public List<ExperienceDTO> getAllExperience() {
-//        return experienceService.getAllExperience();
-//
-//    }
+    @PostMapping("/multi")
+    public ResponseEntity<?> createMultipleEducations(@RequestBody List<EducationDTO> educationDTOs) {
+        String response = educationService.addMultipleEducations(educationDTOs);
+        return ResponseEntity.ok(response);
+    }
 
+    // Uncomment if you need these methods
+    // @GetMapping("/{id}")
+    // public EducationDTO getEducationById(@PathVariable int id) {
+    //     return educationService.getEducationById(id);
+    // }
 
+    // @GetMapping("/all")
+    // public List<EducationDTO> getAllEducation() {
+    //     return educationService.getAllEducation();
+    // }
 }
--- src/main/java/com/humanresourcemanagementsystem/Service/Impl/EducationIMPL.java
+++ src/main/java/com/humanresourcemanagementsystem/Service/Impl/EducationIMPL.java
@@ -1,7 +1,5 @@
 package com.humanresourcemanagementsystem.Service.Impl;
 
-import com.humanresourcemanagementsystem.Dto.AssetDTO;
-import com.humanresourcemanagementsystem.Dto.BenefitDTO;
 import com.humanresourcemanagementsystem.Dto.EducationDTO;
 import com.humanresourcemanagementsystem.Entity.Education;
 import com.humanresourcemanagementsystem.Entity.Person;
@@ -13,8 +11,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class EducationIMPL implements EducationService {
@@ -25,69 +21,37 @@ public class EducationIMPL implements EducationService {
     @Autowired
     private PersonRepository personRepository;
 
-//    @Override
-//    public EducationDTO getEducationById(int id) {
-//        Optional<Education> educationOpt = educationRepository.findById(id);
-//        if (educationOpt.isPresent()) {
-//            Education education = educationOpt.get();
-//            // Fetch associated education information
-//            EducationDTO educationDTO = new EducationDTO();
-//            educationDTO.setEducation_id(education.getEducation_id());
-//            educationDTO.setDegree(education.getDegree());
-//            educationDTO.setInstitution(education.getInstitution());
-//            educationDTO.setMajor(education.getMajor());
-//            educationDTO.setGraduation_start_date(education.getGraduation_start_date());
-//            educationDTO.setGraduation_end_date(education.getGraduation_end_date());
-//            educationDTO.setCreated_at(education.getCreated_at());
-//            educationDTO.setUpdated_at(education.getUpdated_at());
-//
-//            // Fetch associated employee details
-//            Employee employee = benefit.getEmployee();
-//            if (employee != null) {
-//                benefitDTO.setEmployee_id(employee.getEmployeeID());
-//            }
-//            return benefitDTO;
-//        } else {
-//            throw new RuntimeException("Benefit not found with id: " + id);
-//        }
-//    }
+    @Override
+    public String addMultipleEducations(List<EducationDTO> educationDTOs) {
+        for (EducationDTO educationDTO : educationDTOs) {
+            Person person = personRepository.findById(educationDTO.getPerson_id()).orElse(null);
+            if (person == null) {
+                return "Person not found";
+            }
 
-//    @Override
-//    public List<BenefitDTO> getAllBenefit() {
-//        List<Benefit> benefits = benefitRepository.findAll();
-//        return benefits.stream()
-//                .map(benefit -> {
-//                    BenefitDTO benefitDTO = new BenefitDTO();
-//                    benefitDTO.setBenefit_id(benefit.getBenefit_id());
-//                    benefitDTO.setBenefit_type(benefit.getBenefit_type());
-//                    benefitDTO.setCoverage_details(benefit.getCoverage_details());
-//                    benefitDTO.setStart_date(benefit.getStart_date());
-//                    benefitDTO.setEnd_date(benefit.getEnd_date());
-//                    benefitDTO.setCreated_at(benefit.getCreated_at());
-//                    benefitDTO.setUpdated_at(benefit.getUpdated_at());
-//                    // Fetch associated employee details
-//                    Employee employee = benefit.getEmployee();
-//                    if (employee != null) {
-//                        benefitDTO.setEmployee_id(employee.getEmployeeID());
-//                    }
-//                    return benefitDTO;
-//                })
-//                .collect(Collectors.toList());
-//    }
+            Education education = new Education();
+            education.setDegree(educationDTO.getDegree());
+            education.setInstitution(educationDTO.getInstitution());
+            education.setMajor(educationDTO.getMajor());
+            education.setGraduation_start_date(educationDTO.getGraduation_start_date());
+            education.setGraduation_end_date(educationDTO.getGraduation_end_date());
+            education.setCreated_at(new Date());
+            education.setUpdated_at(new Date());
+            education.setPerson(person);
 
+            educationRepository.save(education);
+        }
+        return "Education records added successfully";
+    }
 
     @Override
     public String addEducation(EducationDTO educationDTO) {
-
-        // Fetch and set the person
         Person person = personRepository.findById(educationDTO.getPerson_id()).orElse(null);
         if (person == null) {
             return "Person not found";
         }
 
         Education education = new Education();
-        // Fetch associated education information
-        education.setEducation_id(educationDTO.getEducation_id());
         education.setDegree(educationDTO.getDegree());
         education.setInstitution(educationDTO.getInstitution());
         education.setMajor(educationDTO.getMajor());
@@ -95,8 +59,6 @@ public class EducationIMPL implements EducationService {
         education.setGraduation_end_date(educationDTO.getGraduation_end_date());
         education.setCreated_at(educationDTO.getCreated_at());
         education.setUpdated_at(educationDTO.getUpdated_at());
-        education.setPerson(person); // Correctly set the Person entity
-
         education.setPerson(person);
 
         educationRepository.save(education);
@@ -105,11 +67,13 @@ public class EducationIMPL implements EducationService {
 
     @Override
     public EducationDTO getEducationById(int id) {
+        // Your implementation here
         return null;
     }
 
     @Override
     public List<EducationDTO> getAllEducation() {
+        // Your implementation here
         return List.of();
     }
 }
