9aea1d2d20fbd86e35e6eacd20b8d78970bd836b
Andreas Mueller <andreas.mueller.ak@googlemail.com>
2024-06-11 02:25:51 +0200

    Polished EmployeeDetail Page

--- frontend/hrm/src/Pages/EmployeeDetail.js
+++ frontend/hrm/src/Pages/EmployeeDetail.js
@@ -1,10 +1,20 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderComponent from '../Components/HeaderComponent';
 import Sidebar from '../Components/Sidebar';
+import dateDot from '../images/dateIcon.png';
+import locationDot from '../images/locationIcon.png';
 import './EmployeeDetail.css';
 
 function EmployeeDetail() {
+  const [employeeData, setEmployeeData] = useState({});
   const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/v1/employee/save')
+      .then(response => response.json())
+      .then(data => setEmployeeData(data))
+      .catch(error => console.error('Error fetching employee data:', error));
+  }, []);
 
   const handleEditToggle = () => {
     setIsEditing(!isEditing);
@@ -18,117 +28,136 @@ function EmployeeDetail() {
         <div className="content-area">
           <div className="info-section">
             
-          <div className="info-card profile-info-card">
+            <div className="info-card profile-info-card">
               <button className="edit-button" onClick={handleEditToggle}>Edit</button>
-              <h1 className="employee-name-card">Julia Maier</h1>
-              <span className="badge">Project Manager</span>
+              <h1> Nikhil the Allrounder</h1>  {/* Use this for API maybe-->  className="employee-name-card">{employeeData.firstName} {employeeData.lastName} */}
+              <span className="badge">{employeeData.designation}</span>
               <div className="info-group">
                 <label>Department:</label>
-                <span>Sales & Marketing</span>
+                <span>{employeeData.departmentId}</span>
               </div>
               <div className="info-group">
                 <label>Date of Joining:</label>
-                <span>Jan 19, 2020</span>
+                <span>{employeeData.hireDate}</span>
               </div>
               <div className="info-group">
                 <label>Email:</label>
-                <span>o.williams@gmail.com</span>
+                <span>{employeeData.empEmail}</span>
               </div>
               <div className="info-group">
-                <label>Last Promotion:</label>
-                <span>12 Apr, 2020</span>
+                <label>Designation:</label>
+                <span>{employeeData.Designation}</span>
               </div>
             </div>
-
 
             <div className="info-card salary-info-card">
               <h2>Salary Information</h2>
               <button className="edit-button" onClick={handleEditToggle}>Edit</button>
               <div className="info-group">
                 <label>Salary Basis:</label>
-                <span>Monthly</span>
+                <span>{employeeData.salaryBasis}</span>
               </div>
               <div className="info-group">
                 <label>Salary Amount Per Month:</label>
-                <span>$1300</span>
+                <span>{employeeData.salaryAmount}</span>
               </div>
               <div className="info-group">
                 <label>Effective Date:</label>
-                <span>13/06/2024</span>
+                <span>{employeeData.effectiveDate}</span>
               </div>
               <div className="info-group">
                 <label>Payment Type:</label>
-                <span>Transfer</span>
+                <span>{employeeData.paymentType}</span>
               </div>
               <div className="info-group">
                 <label>Bill Rate:</label>
-                <span>20%</span>
+                <span>{employeeData.billRate}</span>
               </div>
             </div>
-
-         
 
             <div className="info-card personal-info-card">
               <h2>Personal Info</h2>
               <button className="edit-button" onClick={handleEditToggle}>Edit</button>
               <div className="info-group">
                 <label>Gender:</label>
-                <span>Female</span>
+                <span>{employeeData.gender}</span>
               </div>
               <div className="info-group">
                 <label>Date of Birth:</label>
-                <span>12/07/1998</span>
+                <span>{employeeData.dateOfBirth}</span>
               </div>
               <div className="info-group">
                 <label>Phone Number:</label>
-                <span>+49 151 42611442</span>
+                <span>{employeeData.phone}</span>
               </div>
               <div className="info-group">
                 <label>Marital Status:</label>
-                <span>Married</span>
+                <span>{employeeData.maritalStatus}</span>
               </div>
               <div className="info-group">
                 <label>Nationality:</label>
-                <span>German</span>
+                <span>{employeeData.nationality}</span>
               </div>
               <div className="info-group">
                 <label>Street Name:</label>
-                <span>Eppelheimer Str. 12</span>
+                <span>{employeeData.address}</span>
               </div>
               <div className="info-group">
                 <label>Postcode:</label>
-                <span>69115</span>
+                <span>{employeeData.postcode}</span>
               </div>
               <div className="info-group">
                 <label>City:</label>
-                <span>Heidelberg</span>
+                <span>{employeeData.city}</span>
               </div>
             </div>
 
-          
-
             <div className="info-card education-card">
-              <h2>Educations</h2>
+              <h2>Education</h2>
               <button className="edit-button" onClick={handleEditToggle}>Edit</button>
               <div className="education-group">
                 <label>Bachelor of Science in Computer Science</label>
-                <span>University of Technology</span>
-                <span>Graduated May 2017</span>
+                <div className="education-detail">
+                  <img src={locationDot} alt="location icon" className="icon"/>
+                  <span>{employeeData.bachelorUniversity}</span>
+                </div>
+                <div className="education-detail">
+                  <img src={dateDot} alt="date icon" className="icon"/>
+                  <span>Graduated {employeeData.bachelorGraduationDate}</span>
+                </div>
               </div>
               <div className="education-group">
                 <label>Master of Science in Computer Science</label>
-                <span>University of Technology</span>
-                <span>Graduated April 2019</span>
+                <div className="education-detail">
+                  <img src={locationDot} alt="location icon" className="icon"/>
+                  <span>{employeeData.masterUniversity}</span>
+                </div>
+                <div className="education-detail">
+                  <img src={dateDot} alt="date icon" className="icon"/>
+                  <span>Graduated {employeeData.masterGraduationDate}</span>
+                </div>
               </div>
               <div className="education-group">
                 <label>Certification in Full Stack Web Development</label>
-                <span>Coding Academy</span>
-                <span>Graduated June 2020</span>
+                <div className="education-detail">
+                  <img src={locationDot} alt="location icon" className="icon"/>
+                  <span>{employeeData.certificationInstitution}</span>
+                </div>
+                <div className="education-detail">
+                  <img src={dateDot} alt="date icon" className="icon"/>
+                  <span>Graduated {employeeData.certificationDate}</span>
+                </div>
               </div>
               <div className="education-group">
                 <label>Certification as Scrum-Master</label>
-                <span>Coding Academy</span>
-                <span>Graduated June 2020</span>
+                <div className="education-detail">
+                  <img src={locationDot} alt="location icon" className="icon"/>
+                  <span>{employeeData.scrumInstitution}</span>
+                </div>
+                <div className="education-detail">
+                  <img src={dateDot} alt="date icon" className="icon"/>
+                  <span>Graduated {employeeData.scrumDate}</span>
+                </div>
               </div>
             </div>
 
@@ -137,24 +166,42 @@ function EmployeeDetail() {
               <button className="edit-button" onClick={handleEditToggle}>Edit</button>
               <div className="experience-group">
                 <label>Senior Project Manager</label>
-                <span>Frankfurt</span>
-                <span>Aug 2023 - Present</span>
-                <span>Tech Solutions Pro</span>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. When an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <div className="experience-detail">
+                  <img src={locationDot} alt="location icon" className="icon"/>
+                  <span>{employeeData.seniorLocation}</span>
+                </div>
+                <div className="experience-detail">
+                  <img src={dateDot} alt="date icon" className="icon"/>
+                  <span>{employeeData.seniorDate}</span>
+                </div>
+                <span>{employeeData.seniorCompany}</span>
+                <p>{employeeData.seniorDescription}</p>
               </div>
               <div className="experience-group">
                 <label>Assistant Project Manager</label>
-                <span>Köln</span>
-                <span>Jul 2018 - Dec 2019</span>
-                <span>Tech Innovators Ltd.</span>
-                <p>Assisted Project Managers in coordinating project activities, resources, and timelines. Prepared project status reports, tracked progress, and managed project documentation.</p>
+                <div className="experience-detail">
+                  <img src={locationDot} alt="location icon" className="icon"/>
+                  <span>{employeeData.assistantLocation}</span>
+                </div>
+                <div className="experience-detail">
+                  <img src={dateDot} alt="date icon" className="icon"/>
+                  <span>{employeeData.assistantDate}</span>
+                </div>
+                <span>{employeeData.assistantCompany}</span>
+                <p>{employeeData.assistantDescription}</p>
               </div>
               <div className="experience-group">
                 <label>Project Coordinator</label>
-                <span>Mainz</span>
-                <span>Aug 2023 - Present</span>
-                <span>Software Solutions</span>
-                <p>Assisted Project Managers in coordinating project activities and ensuring adherence to timelines. Prepared and delivered project status reports.</p>
+                <div className="experience-detail">
+                  <img src={locationDot} alt="location icon" className="icon"/>
+                  <span>{employeeData.coordinatorLocation}</span>
+                </div>
+                <div className="experience-detail">
+                  <img src={dateDot} alt="date icon" className="icon"/>
+                  <span>{employeeData.coordinatorDate}</span>
+                </div>
+                <span>{employeeData.coordinatorCompany}</span>
+                <p>{employeeData.coordinatorDescription}</p>
               </div>
             </div>
 
@@ -162,23 +209,12 @@ function EmployeeDetail() {
               <h2>Skills</h2>
               <button className="edit-button" onClick={handleEditToggle}>Edit</button>
               <div className="skills">
-                <span>Project Management</span>
-                <span>Team Leadership</span>
-                <span>Data Agile Methodologies</span>
-                <span>Risk Management</span>
-                <span>Budgeting and Financial Management</span>
-                <span>Stakeholder Communication</span>
-                <span>Process Improvement</span>
-                <span>Scope Management</span>
-                <span>Quality Assurance</span>
-                <span>Presentation Skills</span>
-                <span>Conflict Resolution</span>
-                <span>Data Analytics Tools</span>
-                <span>Trello</span>
-                <span>JIRA</span>
-                <span>Microsoft Project</span>
+                {employeeData.skills && employeeData.skills.map((skill, index) => (
+                  <span key={index}>{skill}</span>
+                ))}
               </div>
             </div>
+
           </div>
         </div>
       </div>
